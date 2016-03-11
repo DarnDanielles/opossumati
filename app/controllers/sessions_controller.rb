ee672a848c177e0f4db835172f970fb116da0f10
kscotteng <kscott@gmail.com>
2016-03-11 16:22:47 -0500

    Create login html, create seed, edit routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = Author.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    author = Author.find_by_email(params[:email])
+    if author && author.authenticate(params[:password])
       session[:user_id] = author.id
       redirect_to root_path, notice: "You have successfully logged in"
     else
