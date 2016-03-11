50061898cdc6d3753f9e10e5b56d8043cbf07ad2
kscotteng <kscott@gmail.com>
2016-03-11 11:39:20 -0500

    Add login authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,19 @@ class SessionsController < ApplicationController
 
 
   def create
-
+    user = Author.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = author.id
+      redirect_to root_path, notice: "You have successfully logged in"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render "New"
+    end
   end
 
-
   def destroy
-
+    session[:user_id] = nil
+    redirect_to login_path, notice: "Logged out, have a nice day!"
   end
 
 end
