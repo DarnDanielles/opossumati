f4dc7463a2f4dff3959c30fefba4646a3b8d81d5
kscotteng <kscott@gmail.com>
2016-03-11 11:44:01 -0500

    Create logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def logged_in?
+    if session[:user_id].nil?
+      redirect_to login_path, notice: "Please login first" unless session[:user_id]
+    end
+  end
+
+
 end
