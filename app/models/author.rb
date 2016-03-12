6585459bbe526b67fb9f16c79b9f732d6f4ff55c
alexanderallis <alexandreallis@gmail.com>
2016-03-12 11:33:57 -0500

    first commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ActiveRecord::Base
   has_many :surveys
 
-  validates :password, prescense: true
-  validates :email, prescense: true, uniqueness: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
 end
