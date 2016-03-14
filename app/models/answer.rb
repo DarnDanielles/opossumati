f26f26503529455e7827b4734cd840112499d081
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-03-13 23:01:28 -0400

    Discover all the broken things.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,12 @@
 class Answer < ActiveRecord::Base
   belongs_to :taker
   belongs_to :question
+  validate :submit_require
+
+
+  def submit_require
+    Answer.all.any? { |a| a.response.blank? && a.question.required }
+    errors.add(:required, "Survey cannot be submitted without completing all required questions")
+  end
+
 end
