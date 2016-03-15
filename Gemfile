3048f211a2baeda2a5d2d6b554c25f52a1a70744
Daniel Buompane <danielbuompane@gmail.com>
2016-03-14 22:50:30 -0400

    Remove footnotes gem

--- Gemfile
+++ Gemfile
@@ -35,7 +35,6 @@ gem 'rails_12factor'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  gem 'rails-footnotes'
   gem 'sqlite3'
 end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,19 @@ class SessionsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    author = Author.find_by_email(params[:email])
-    if author && author.authenticate(params[:password])
-      session[:user_id] = author.id
-      redirect_to root_path, notice: "You have successfully logged in"
-    else
-      flash.now[:alert] = "Invalid email or password"
-      render "new"
+
+    def create
+      author = Author.find_by_email(params[:email])
+      if author && author.authenticate(params[:password])
+        session[:user_id] = author.id
+        redirect_to root_path, notice: "You have successfully logged in"
+      else
+        flash.now[:alert] = "Invalid email or password"
+        render "new"
+      end
     end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to login_path, notice: "Logged out, have a nice day!"
-  end
-
+    def destroy
+      session[:user_id] = nil
+      redirect_to login_path, notice: "Logged out, have a nice day!"
+    end
 end
